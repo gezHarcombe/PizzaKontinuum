006dc1050de765380915cbc558dda30c8cc2d039
btabram <btabram@users.noreply.github.com>
2017-02-20 21:35:56 +0000

    It works now

--- code/score.py
+++ code/score.py
@@ -3,6 +3,7 @@ import sys
 from data import R, C
 
 filename = str(sys.argv[1])
+filename = filename + '.out'
 data = np.loadtxt(filename, skiprows=1)
 score = np.sum((data[:,2]+1-data[:,0]) * (data[:,3]+1-data[:,1]))
 print(score / (R * C))
--- code/score.sh
+++ code/score.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 python main.py $1 > $1.out
-python score.py $1.out
+python score.py $1
