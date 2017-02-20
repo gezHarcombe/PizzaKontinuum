f3871f9e56988a3e8494ef4a6374fc2b91c538a1
knutsvk <ksk38@cam.ac.uk>
2017-02-20 20:32:37 +0000

    added pos class to main.py

--- code/main.py
+++ code/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 from data import R, C, L, H, A
+from gez import Pos
 from knut import divide 
 
 pizza = A.copy()
-pos = [0,0,R-1,C-1]
+pos = Pos(0, 0, R-1, C-1)
 slices = []
 divide(pizza, pos, slices)
 
 print(len(slices))
 for s in slices: 
-    print(s[0], s[1], s[2], s[3])
+    s.print()
