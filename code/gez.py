b5ed1e3c164d5ece216165984ed4bdb30dfe8dd4
knutsvk <ksk38@cam.ac.uk>
2017-02-20 19:16:52 +0000

    Actually did it this time

--- code/gez.py
+++ code/gez.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from funcs import count_tomatoes, count_mushrooms, read_pizza
-from main import R, C, L, H, A
 
 """ Function takes a slice and returns ratio of the rarer ingredient to 
     the more abundant ingredient
