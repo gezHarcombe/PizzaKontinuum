9ac93b11c6f7c6165f339f0765859e6feecd13f0
knutsvk <ksk38@cam.ac.uk>
2017-02-20 20:21:25 +0000

    finalised main file

--- code/main.py
+++ code/main.py
@@ -3,9 +3,10 @@ from data import R, C, L, H, A
 from knut import divide 
 
 pizza = A.copy()
-pos = [[0,0],[R,C]]
+pos = [0,0,R-1,C-1]]
 slices = []
 divide(pizza, pos, slices)
 
+print(len(slices))
 for s in slices: 
-    print(s)
+    print(s[0], s[1], s[2], s[3])
