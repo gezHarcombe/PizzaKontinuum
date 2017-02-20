f12797671543415a6f2a32a7b09cade9571f8170
LiamPattinson <liampattinson@gmail.com>
2017-02-20 18:34:50 +0000

    funcs updated

--- code/funcs.py
+++ code/funcs.py
@@ -10,25 +10,29 @@ def read_pizza(filename):
     L = int(info[2])
     H = int(info[3])
 
-    A = np.empty((R, C), dtype=int)
+    A = np.empty((R, C), dtype=float)
     for i in range(R):
         row = f.readline()
         for j in range(C):
             if row[j] == 'T':
-                A[i,j] = 1
+                A[i,j] = 1.0
             else:
-                A[i,j] = 0
+                A[i,j] = 0.0
     f.close()
 
     return R, C, L, H, A
 
-def remove_slice( pizza, rows, cols):
+def remove_slice_copy( pizza, rows, cols):
     result = np.copy(pizza)
-    result[ rows[0]:rows[1], cols[0]:cols[1]] = 0
+    result[ rows[0]:rows[1]+1, cols[0]:cols[1]+1 ]= -1e-50
+    return result
+
+def remove_slice(pizza,rows,cols):
+    pizza[ rows[0]:rows[1]+1, cols[0]:cols[1]+1 ]= -1e-50
     return result
 
 def count_tomatoes(arr):
-    return sum(arr)
+    return np.sum(arr)
 
 def count_mushrooms(arr):
-    return arr.size - sum(arr)
+    return arr.size - count_tomatoes(arr)
--- code/main.py
+++ code/main.py
@@ -1,6 +1,10 @@
 import numpy as np
-from funcs import read_pizza
+from funcs import *
 
 R, C, L, H, A = read_pizza('../pizzas/example.in')
 
 print(A)
+B = remove_slice_copy(A,[0,1],[0,1])
+print(B)
+print(count_tomatoes(A))
+print(count_tomatoes(B))
