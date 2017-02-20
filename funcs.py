570fcee28b805131a523ece50eaebc712004cef1
knutsvk <ksk38@cam.ac.uk>
2017-02-20 17:48:42 +0000

    input files and read pizza function

---
+++ funcs.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def read_pizza(filename):
+
+    f = open(filename, 'r')
+
+    info = f.readline().split()
+    R = int(info[0]) 
+    C = int(info[1]) 
+    L = int(info[2]) 
+    H = int(info[3]) 
+
+    A = np.empty((R, C), dtype=int)
+    for i in range(R):
+        row = f.readline()
+        for j in range(C):
+            if row[j] == 'T':
+                A[i,j] = 1
+            else:
+                A[i,j] = 0
+    f.close()
+
+    return R, C, L, H, A
